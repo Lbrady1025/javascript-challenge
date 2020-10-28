01d7b77750a262afa327befd53d2219fe32ad586
Leah Brady <brady.leah@gmail.com>
2020-10-28 19:39:41 -0400

    read data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach( sighting => {
+    console.log(sighting);
+});
+
+// let tr = d3.select("thead > tr");
+
+tableData.forEach( sighting => {
+    let tr = tbody.append("tr");
+    Object.entries(sighting).forEach( function ([key,value]) {
+        let cell=tr.append("td");
+        cell.text(value);
+    });
+});
