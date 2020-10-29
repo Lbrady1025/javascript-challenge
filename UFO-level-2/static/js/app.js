6b96dcc3c276ec493a80ebe31e58c24230662b9d
Leah Brady <brady.leah@gmail.com>
2020-10-28 20:26:46 -0400

    add level 1 code to level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,47 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach( sighting => {
+    console.log(sighting);
+});
+
+tableData.forEach( sighting => {
+    let tr = tbody.append("tr");
+    Object.entries(sighting).forEach( function ([key,value]) {
+        let cell=tr.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+// var datetime = d3.select("#datetime");
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+
+function runEnter () {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    console.log(filteredData);
+
+    tbody.html("");
+
+    filteredData.forEach( sighting => {
+        let tr = tbody.append("tr");
+        Object.entries(sighting).forEach( function ([key,value]) {
+            let cell=tr.append("td");
+            cell.text(value);
+        });
+    });
+};
